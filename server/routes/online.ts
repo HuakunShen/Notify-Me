4543e73728cdd20af445b1e75729e9d90550386b
Huakun Shen <huakun.shen@huakunshen.com>
2022-09-26 19:14:33 -0400

    Update

--- server/routes/online.ts
+++ server/routes/online.ts
@@ -1,4 +1,8 @@
 export default defineEventHandler((event) => {
-  console.log(event.req.socket.remoteAddress);
-  return { online: true, remoteAddress: event.req.socket.remoteAddress };
+  console.log(event.req.headers["x-forwarded-for"]);
+  const ip =
+    event.req.headers["x-forwarded-for"] || // From proxy headers, can be spoofed if you don't have a proxy in front of your app, so drop it if your app is naked.
+    event.req.connection.remoteAddress ||
+    event.req.socket.remoteAddress; // socket is an alias to connection, just delete this line
+  return { online: true, remoteAddress: ip };
 });
